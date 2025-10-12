269932c5138f611a67dbc2bfe40b7a69e94324f4
bs1cx <doukan.krdas@gmail.com>
2025-10-12 23:43:01 +0300

    Python 3.13 uyumluluğu: WebSocket bağımlılıkları kaldırıldı, eventlet sorunu çözüldü

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo " Build başlıyor..."
+echo "Build başlıyor..."
+
+# Gereksiz bağımlılıkları kaldır, sadece gerekli olanları yükle
 pip install -r requirements.txt
-echo " Build tamamlandı!"
+
+echo "Build tamamlandı!"
